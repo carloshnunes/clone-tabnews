99cc6c2fa9e718b4e5691edeb1777dac963c8513
carloshnunes <110049855+carloshnunes@users.noreply.github.com>
2024-05-15 16:02:24 +0000

    troquei a frase mais uma vez

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Amor, se tu tá lendo isso tá dando tudo certo e eu te amo, logo o trampo vem!
+    return <h1>Po, sei lá, eu nem sei mais se eu quero estudar tanto assim, hoje acordei bem mas quase me entreguei a depressao aqui, muito difícil ser um ser humano
+        normal e funcional...
     </h1>
 }
 
